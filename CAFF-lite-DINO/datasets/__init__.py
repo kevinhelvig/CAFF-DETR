0d43f1584eb6377a982feed6584b4ce7b571d6ac
Kevin Helvig <129502553+kevinhelvig@users.noreply.github.com>
2024-11-09 11:18:31 +0100

    Update __init__.py

--- CAFF-lite-DINO/datasets/__init__.py
+++ CAFF-lite-DINO/datasets/__init__.py
@@ -1,9 +1,10 @@
+# khelvig
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import torch.utils.data
 import torchvision
 
 from .coco import build as build_coco
-from .coco import  build_RGBT
+from .coco import  build_RGBT_LLVIP, build_RGBT_FLIR
 
 def get_coco_api_from_dataset(dataset):
     for _ in range(10):
@@ -18,10 +19,11 @@ def get_coco_api_from_dataset(dataset):
 
 def build_dataset(image_set, args):
     print(args.dataset_file) 
-    if args.dataset_file == 'coco':
-        return build_RGBT(image_set, args)
-    if args.dataset_file == 'fusion':
-        return build_RGBT(image_set, args)    
+    if args.dataset_file == 'flir_fusion':
+        return build_RGBT_FLIR(image_set, args)
+    if args.dataset_file == 'llvip_fusion':
+        return build_RGBT_LLVIP(image_set, args)   
+         
     if args.dataset_file == 'coco_panoptic':
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
